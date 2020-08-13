df1b379add1d82f8794ce8d55e507f58f75a8033
Kirikmelet <troy.dwijanto2@gmail.com>
2020-08-13 19:13:46 +0700

    Finished emacs config
    Finally rebuilt it :D

---
+++ emacs/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (projectile which-key use-package magit lsp-ui lsp-ivy ivy-dired-history gruvbox-theme eldoc-overlay eldoc-eval dashboard dap-mode company-box all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs/init.el
@@ -0,0 +1,22 @@
+;; Error checking
+(setq debug-on-error t)
+
+;; Paths to load
+(defconst emacs-dir (substitute-in-file-name "$HOME/.config/emacs/"))
+(defconst config-dir (concat emacs-dir "/config"))
+
+
+;; Loads config files
+(add-to-list 'load-path config-dir)
+
+
+
+;; Loads packages
+(require 'package-configfile)
+
+;; better-defaults by Phil Hagelberg 
+;; https://git.sr.ht/~technomancy/better-defaults/tree/master/better-defaults.el
+(require 'better-defaults)
+
+;; For emacs daemon
+(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
